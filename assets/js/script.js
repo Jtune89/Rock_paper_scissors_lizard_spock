cfd15cb94ffdcc91c84d05e3e71981b20b8f5218
Jtune89 <Joseph.tune1@gmail.com>
2022-05-08 14:42:39 +0000

    js changes - not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,10 @@
-const selectionButtons = document.querySelectorAll ('[data-selection')
-const SELECTIONS = [
-    {
-        name: 'rock',
-        beats:'scissors'
-    },
-    {
-        name: 'scissors',
-        beats:'paper'
-    },
-    {
-        name: 'paper',
-        beats:'rock'
-    },
-    {
-        name: 'lizard',
-        beats:'spock'
-    },
-    {
-        name: 'spock',
-        beats:'rock'
-    },
-]
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userChoice
 
-selectionButtons.forEach (selectionButton => {
-    selectionButton.addEventListener ('click', e => {
-       const selectionName = selectionButton.dataset.selection
-       const selection = SELECTIONS.find(selection => selection.name === selectionName,)
-       makeSelection (selection)
-    })
-})
-
-function makeSelection(selection) {
-    const computerSelection = randomSelection ()
-    const yourWinner = isWinner (selection, computerSelection)
-    const computerWinner = isWinner (computerSelection.name)
-
-    addSelectionResult (computerSelection, computerWinner)
-    addSelectionResult (selection, yourWinner)
-
-    {console.log(isWinner)};
-}
-
-
-function addSelectionResult (selection, winner){
-
-}
-
-function isWinner (selection, opponentSelection){
-    return selection.beats === opponentSelection.name
-}
-
-function randomSelection () {
-    const randomIndex = Math.floor (Math.random() * SELECTIONS.length)
-    return SELECTIONS [randomIndex]
-}
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+  userChoice = e.target.id
+  userChoiceDisplay.innerHTML = userChoice
+}))
